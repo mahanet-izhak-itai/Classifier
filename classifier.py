4e5d8743af19fa5d53844d5d1933f9c7caf5c1f5
itai <itaimain@gmail.com>
2016-05-15 20:58:56 +0300

    Draw a path

--- classifier.py
+++ classifier.py
@@ -3,6 +3,8 @@
 import numpy
 import cv2
 
+buf = []
+
 def detect(img):
     cascade = cv2.CascadeClassifier("output/cascade.xml")
     rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
@@ -13,8 +15,15 @@ def detect(img):
     return rects, img
 
 def box(rects, img):
+    global buf
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+        buf.append([(x1 + x2) / 2, (y1 + y2) / 2])
+    if 10 < len(buf):
+        buf = buf[-10:]
+    for point in buf:
+        cv2.circle(img,(point[0],point[1]), 5, (0,0,255), -1)
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3,400)
@@ -22,6 +31,7 @@ cap.set(4,300)
 
 while(True):
     ret, img = cap.read()
+    img = cv2.flip(img, 1)
     rects, img = detect(img)
     box(rects, img)
     cv2.imshow("frame", img)
